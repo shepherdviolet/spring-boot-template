3f8174edc682366bc120253767a0c974cb904d62
shepherdviolet <shepherdviolet@163.com>
2023-12-19 11:38:41 +0800

    1> Jasypt demo

--- web/demo/src/main/java/com/github/shepherdviolet/webdemo/demo/jasypt/controller/JasyptController.java
+++ web/demo/src/main/java/com/github/shepherdviolet/webdemo/demo/jasypt/controller/JasyptController.java
@@ -1,5 +1,11 @@
 package com.github.shepherdviolet.webdemo.demo.jasypt.controller;
 
+import com.github.shepherdviolet.glacimon.java.conversion.Base64Utils;
+import com.github.shepherdviolet.glacimon.java.crypto.PEMEncodeUtils;
+import com.github.shepherdviolet.glacimon.java.crypto.RSAKeyGenerator;
+import com.ulisesbocchio.jasyptspringboot.encryptor.SimpleAsymmetricConfig;
+import com.ulisesbocchio.jasyptspringboot.encryptor.SimpleAsymmetricStringEncryptor;
+import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.slf4j.Logger;
@@ -7,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.spec.InvalidKeySpecException;
 
 /**
  * jasypt-spring-boot-starter示例: 参数加密
@@ -17,7 +25,7 @@ public class JasyptController {
 
     private static final Logger logger = LoggerFactory.getLogger(JasyptController.class);
 
-    @Value("${jasypt-test.param1}")
+    @Value("${jasypt.test-param1}")
     private String param1;
 
     /**
@@ -29,6 +37,56 @@ public class JasyptController {
     }
 
     /* ****************************************************************************************************************
+     * 非对称加密方案
+     **************************************************************************************************************** */
+
+    public static void main(String[] args) throws InvalidKeySpecException {
+        /*
+         * 生成RSA公私钥, 私钥配置在运行环境的启动参数/环境变量中, 公钥提供给开发人员.
+         * 对密码强度要求不高的情况下可以考虑用1024位的密钥, 短一些. 要求高的用2048.
+         *
+         * 公钥提供给开发使用, 可以直接写在工具程序里, 供开发加密信息.
+         *
+         * 私钥配置在启动参数或环境变量中:
+         * -Djasypt.encryptor.privateKeyString=......
+         * 也可以将私钥放置在文件中:
+         * jasypt.encryptor.privateKeyLocation=/home/yourname/privatekey.pem
+         * 可以设置私钥格式, 默认DER, 可以设置成PEM
+         * jasypt.encryptor.privateKeyFormat=DER
+         * (本示例工程中, 配在了application.yaml里, 为了测试方便, 生产环境不要这么做)
+         */
+//        RSAKeyGenerator.RSAKeyPair keyPair = RSAKeyGenerator.generateKeyPair(1024);
+//        String publicKeyDer = Base64Utils.encodeToString(keyPair.getX509EncodedPublicKey()); //DER格式公钥 (jasypt也支持PEM)
+//        String privateKeyDer = Base64Utils.encodeToString(keyPair.getPKCS8EncodedPrivateKey()); //DER格式私钥 (jasypt也支持PEM)
+//        System.out.println("\nPUBLIC: \n" + publicKeyDer);
+//        System.out.println("\nPRIVATE: \n" + privateKeyDer);
+//        System.out.println("\nPRIVATE PEM: \n" + PEMEncodeUtils.rsaPrivateKeyToPEMEncoded(privateKeyDer));
+
+        /*
+         * 用公钥加密信息
+         */
+        SimpleAsymmetricConfig config = new SimpleAsymmetricConfig();
+        config.setPublicKey("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCL1O9+qYUlgZmWZKSGo6mxPz4E7JPx4uYuQdPpwDyGqGk8oR4ZaISpn26GBwlDmz0iCUj4+xQa0K5Sh4/Ie97w2C4NiCkJNOOqrfpgIbRLnAlNH5K83NBuW6UD0hEApqQkTrn7ZpHrpmySsGTbzQ5cJyyM+MMkQaPtnFsOjpS2YwIDAQAB");
+
+//        config.setKeyFormat(AsymmetricCryptography.KeyFormat.PEM);
+//        config.setPublicKey("-----BEGIN RSA PUBLIC KEY-----\n" +
+//                "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCL1O9+qYUlgZmWZKSGo6mxPz4E\n" +
+//                "7JPx4uYuQdPpwDyGqGk8oR4ZaISpn26GBwlDmz0iCUj4+xQa0K5Sh4/Ie97w2C4N\n" +
+//                "iCkJNOOqrfpgIbRLnAlNH5K83NBuW6UD0hEApqQkTrn7ZpHrpmySsGTbzQ5cJyyM\n" +
+//                "+MMkQaPtnFsOjpS2YwIDAQAB\n" +
+//                "-----END RSA PUBLIC KEY-----");
+
+        StringEncryptor encryptor = new SimpleAsymmetricStringEncryptor(config);
+        String message = "The parameter that need to be protected";
+        String encrypted = encryptor.encrypt(message);
+
+        /*
+         * 密文加上前缀后缀, 就可以作为参数设置到Spring配置/启动参数里了
+         */
+        System.out.println("ENC(" + encrypted + ")");
+    }
+
+    /* ****************************************************************************************************************
      * 对称加密方案
      **************************************************************************************************************** */
 
@@ -36,61 +94,62 @@ public class JasyptController {
      * 生成密码, 加密信息
      * @param args
      */
-    public static void main(String[] args) {
-        /*
-         * 生成和保管密码:
-         * 建议用工具生成一个密码(例如https://www.sexauth.com/)
-         * 注意!!! 这个密码不要直接放在程序配置文件里, 应该通过启动参数/环境变量等方式在运行环境设置.
-         *
-         * 例如在启动参数里:
-         * -Djasypt.encryptor.password=PRGkutx3FfsCYtyvgL9OjHyHQ7SkxGL
-         */
-        String password = "IPRGkutx3FfsCYtyvgL9OjHyHQ7SkxGL";
-        /*
-         * 在这里填入需要用密码保护的参数原文
-         */
-        String message = "The parameter that need to be protected";
-        /*
-         * 用jasypt默认算法加密信息, 如果你更换了加密算法, 则要用对应的加密算法加密
-         */
-        String messageEncrypted = encryptor(password).encrypt(message);
-        /*
-         * 为了避免解密出来的数据和原文不同, 这里重新解密成原文做个对比
-         */
-        String messageRaw = encryptor(password).decrypt(messageEncrypted);
-        if (!message.equals(messageRaw)){
-            throw new RuntimeException("Encrypt failed, decrypted messsage: " + messageRaw);
-        }
-        /*
-         * 密文加上前缀后缀, 就可以作为参数设置到Spring配置/启动参数里了
-         *
-         * 例如在application.properties里:
-         * param-key=ENC(cABV59oATrHNGVZ33GBDHIBb4LdC7naVuExv9rdJ2dtnYSS6WhK/88KRunZavB1M)
-         *
-         * 例如在启动参数里:
-         * -Dparam-key=ENC(cABV59oATrHNGVZ33GBDHIBb4LdC7naVuExv9rdJ2dtnYSS6WhK/88KRunZavB1M)
-         */
-        System.out.println("ENC(" + messageEncrypted + ")");
-    }
-
-    /**
-     * jasypt-spring-boot-starter 默认的加密器.
-     * 如果配置了自定义加密器或者参数, 请相应调整.
-     */
-    public static StandardPBEStringEncryptor encryptor(String password) {
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(password);
-        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setProviderName(null);
-        config.setProviderClassName(null);
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
-        config.setStringOutputType("base64");
-        encryptor.setConfig(config);
-        return encryptor;
-    }
+//    public static void main(String[] args) {
+//        /*
+//         * 生成和保管密码:
+//         * 建议用工具生成一个密码(例如https://www.sexauth.com/)
+//         * 注意!!! 这个密码不要直接放在程序配置文件里, 应该通过启动参数/环境变量等方式在运行环境设置.
+//         * (本示例工程中, 配在了application.yaml里, 为了测试方便, 生产环境不要这么做)
+//         *
+//         * 例如在启动参数里:
+//         * -Djasypt.encryptor.password=PRGkutx3FfsCYtyvgL9OjHyHQ7SkxGL
+//         */
+//        String password = "IPRGkutx3FfsCYtyvgL9OjHyHQ7SkxGL";
+//        /*
+//         * 在这里填入需要用密码保护的参数原文
+//         */
+//        String message = "The parameter that need to be protected";
+//        /*
+//         * 用jasypt默认算法加密信息, 如果你更换了加密算法, 则要用对应的加密算法加密
+//         */
+//        String messageEncrypted = encryptor(password).encrypt(message);
+//        /*
+//         * 为了避免解密出来的数据和原文不同, 这里重新解密成原文做个对比
+//         */
+//        String messageRaw = encryptor(password).decrypt(messageEncrypted);
+//        if (!message.equals(messageRaw)){
+//            throw new RuntimeException("Encrypt failed, decrypted messsage: " + messageRaw);
+//        }
+//        /*
+//         * 密文加上前缀后缀, 就可以作为参数设置到Spring配置/启动参数里了
+//         *
+//         * 例如在application.properties里:
+//         * param-key=ENC(cABV59oATrHNGVZ33GBDHIBb4LdC7naVuExv9rdJ2dtnYSS6WhK/88KRunZavB1M)
+//         *
+//         * 例如在启动参数里:
+//         * -Dparam-key=ENC(cABV59oATrHNGVZ33GBDHIBb4LdC7naVuExv9rdJ2dtnYSS6WhK/88KRunZavB1M)
+//         */
+//        System.out.println("ENC(" + messageEncrypted + ")");
+//    }
+//
+//    /**
+//     * jasypt-spring-boot-starter 默认的加密器.
+//     * 如果配置了自定义加密器或者参数, 请相应调整.
+//     */
+//    public static StandardPBEStringEncryptor encryptor(String password) {
+//        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+//        config.setPassword(password);
+//        config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+//        config.setKeyObtentionIterations("1000");
+//        config.setPoolSize("1");
+//        config.setProviderName(null);
+//        config.setProviderClassName(null);
+//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+//        config.setIvGeneratorClassName("org.jasypt.iv.RandomIvGenerator");
+//        config.setStringOutputType("base64");
+//        encryptor.setConfig(config);
+//        return encryptor;
+//    }
 
 }
---
+++ web/demo/src/main/resources/config/demo/jasypt/README.md
@@ -0,0 +1,45 @@
+# jasypt私钥文件 (非对称模式专用)
+
+* 生产环境的私钥文件千万不要放在程序里!!! (这里只是为了示例方便)
+* 生产环境的私钥文件千万不要放在程序里!!! (这里只是为了示例方便)
+* 生产环境的私钥文件千万不要放在程序里!!! (这里只是为了示例方便)
+
+## 启动参数设置私钥 (DER格式)
+
+* 在生产环境启动脚本中增加启动参数:
+
+```
+-Djasypt.encryptor.privateKeyString=MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIvU736phSWBmZZkpIajqbE/PgTsk/Hi5i5B0+nAPIaoaTyhHhlohKmfboYHCUObPSIJSPj7FBrQrlKHj8h73vDYLg2IKQk046qt+mAhtEucCU0fkrzc0G5bpQPSEQCmpCROuftmkeumbJKwZNvNDlwnLIz4wyRBo+2cWw6OlLZjAgMBAAECgYAw2o9mMHHtXa88ZSM6SxnxbEgNzl4OB5kmFiekpl4/Kb3CygPLGsImxSYHO5QYA7fDGX1eR9KJX9lXyjcI84Y7E4w0XcGQdSuCILMFcjUlhNtJJEnnzledEKrbysYajVwy7jY0EM6RWs0PVpOygLeqP+Vvwnns2Q3ypQ8IhHb5EQJBAPwX6ssy3VYn5bOTiAqRNmPd6CyVcNebU6mPpwdJKb3SuaVcqhlHLsonqwfWQlh8gHGSsISPa6le7TeC2w5ifmsCQQCN/6nxxJVcb67LL08U+PrsPrEgcAqZtqA4pEQ7jz15ZVqUVR6CPzV13L7rtOzhL70OrfqNhKoPdLwGaufCKrXpAkEAuSa/1fpHwi2PcbMbqdc5gWPMUGJ5/IEik1jkrl83/yk0HJXQgLxdSzCTVzAwjljy5Xd9mf7UbhNAWxMK3KfOfQJALIM4gtFdAN0Bri/mWmyyO9xrKf/1Uros/5R+zyzX2HYtLtJ//dRSrd/E+Z59oxmT6kYfhL1RkgbF6j0Y6YT6AQJAR/HHgWG29Clwx+bwIvsJLKckYYmZudTMKNrCnwUbUt8b5DVpKtXYyUc5hRVWNTI58ns82N2AYrAwXqqgIZ5fXQ==
+```
+
+## 文件存放私钥 (PEM格式)
+
+* 在程序application.yaml中配置
+
+```
+jasypt:
+  encryptor:
+    privateKeyFormat: PEM
+    privateKeyLocation: /home/yourname/jasypt_private.pem
+```
+
+* 编辑文件: /home/yourname/jasypt_private.pem
+
+```
+-----BEGIN RSA PRIVATE KEY-----
+MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIvU736phSWBmZZk
+pIajqbE/PgTsk/Hi5i5B0+nAPIaoaTyhHhlohKmfboYHCUObPSIJSPj7FBrQrlKH
+j8h73vDYLg2IKQk046qt+mAhtEucCU0fkrzc0G5bpQPSEQCmpCROuftmkeumbJKw
+ZNvNDlwnLIz4wyRBo+2cWw6OlLZjAgMBAAECgYAw2o9mMHHtXa88ZSM6SxnxbEgN
+zl4OB5kmFiekpl4/Kb3CygPLGsImxSYHO5QYA7fDGX1eR9KJX9lXyjcI84Y7E4w0
+XcGQdSuCILMFcjUlhNtJJEnnzledEKrbysYajVwy7jY0EM6RWs0PVpOygLeqP+Vv
+wnns2Q3ypQ8IhHb5EQJBAPwX6ssy3VYn5bOTiAqRNmPd6CyVcNebU6mPpwdJKb3S
+uaVcqhlHLsonqwfWQlh8gHGSsISPa6le7TeC2w5ifmsCQQCN/6nxxJVcb67LL08U
++PrsPrEgcAqZtqA4pEQ7jz15ZVqUVR6CPzV13L7rtOzhL70OrfqNhKoPdLwGaufC
+KrXpAkEAuSa/1fpHwi2PcbMbqdc5gWPMUGJ5/IEik1jkrl83/yk0HJXQgLxdSzCT
+VzAwjljy5Xd9mf7UbhNAWxMK3KfOfQJALIM4gtFdAN0Bri/mWmyyO9xrKf/1Uros
+/5R+zyzX2HYtLtJ//dRSrd/E+Z59oxmT6kYfhL1RkgbF6j0Y6YT6AQJAR/HHgWG2
+9Clwx+bwIvsJLKckYYmZudTMKNrCnwUbUt8b5DVpKtXYyUc5hRVWNTI58ns82N2A
+YrAwXqqgIZ5fXQ==
+-----END RSA PRIVATE KEY-----
+```
