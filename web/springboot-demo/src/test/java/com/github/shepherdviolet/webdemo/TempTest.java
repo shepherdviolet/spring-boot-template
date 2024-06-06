a092b2ba32e66542684de2f0ea4054c202b674cc
shepherdviolet <shepherdviolet@163.com>
2024-06-06 22:39:58 +0800

    1> Upgrade gradle 8.8: Adjust the Gradle script to adapt to Gradle9

---
+++ web/springboot-demo/src/test/java/com/github/shepherdviolet/webdemo/TempTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022-2022 S.Violet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project GitHub: https://github.com/shepherdviolet/glacimon
+ * Email: shepherdviolet@163.com
+ */
+
+package com.github.shepherdviolet.webdemo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 临时使用的测试案例
+ * Gradle9:每个module必须要有个Test,不然Gradle9可能会报错:https://docs.gradle.org/8.8/userguide/upgrading_version_8.html#test_task_fail_on_no_test_executed
+ *
+ * @author shepherdviolet
+ */
+public class TempTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
