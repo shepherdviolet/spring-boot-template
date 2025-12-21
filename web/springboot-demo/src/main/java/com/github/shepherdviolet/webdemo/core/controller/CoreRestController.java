95cec8c1d663d08be7e7ab6415d7c81134b70cd6
shepherdviolet <shepherdviolet@163.com>
2025-12-21 22:17:58 +0800

    1> redirect demo

--- web/springboot-demo/src/main/java/com/github/shepherdviolet/webdemo/core/controller/CoreRestController.java
+++ web/springboot-demo/src/main/java/com/github/shepherdviolet/webdemo/core/controller/CoreRestController.java
@@ -152,6 +152,18 @@ public class CoreRestController {
         return null;
     }
 
+    /**
+     * redirect relative (相对路径).
+     * 可以开启'forward-headers-strategy: NATIVE', 也可以不开启, 看需求, 参考笔记'spring-MVC-重定向+代理环境协议适配.txt'
+     * 可以开启'use-relative-redirects: true', 也可以不开启, 看需求, 参考笔记'spring-MVC-重定向+代理环境协议适配.txt'
+     * http://localhost:8000/core/redirect-relative
+     */
+    @RequestMapping("/redirect-relative")
+    public byte[] redirectRelative(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/core/get/wildcard/somepath");
+        return null;
+    }
+
 //    @Autowired
 //    private GlaciHttpClient httpClient;
 //    @Autowired
